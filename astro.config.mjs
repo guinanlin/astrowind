e6e02803193a0ca876a5b4139a356b3b009362a3
hawk <guinan.lin@foxmail.com>
2024-05-06 17:50:47 +0800

    update,test

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,7 +25,7 @@ const whenExternalScripts = (items = []) =>
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  site: 'https://fis5.pages.github.io',
+  site: 'https://fis5.pages.github.io', //site
 
   output: 'static',
 
