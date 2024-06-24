d2fdf7603435a700faf80db24e8dda2b46fe4e83
hawk <guinan.lin@foxmail.com>
2024-06-24 22:43:06 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,29 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.datangyuan.cn",
-  integrations: [tailwind({ configFile: "./tailwind.config.mjs" }),vue({jsx: true, reactivityTransform: true,appEntrypoint: '/src/main.js',})],
+  integrations: [
+    tailwind({ configFile: "./tailwind.config.mjs" }),
+    icon({
+      include: {
+        tabler: ['*'],
+        'flat-color-icons': [
+          'template',
+          'gallery',
+          'approval',
+          'document',
+          'advertising',
+          'currency-exchange',
+          'voice-presentation',
+          'business-contact',
+          'database',
+        ],
+      },
+    }),
+    vue({jsx: true, reactivityTransform: true,appEntrypoint: '/src/main.js',})
+  ],
 });
