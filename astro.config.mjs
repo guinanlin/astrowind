2cc629b655859412212a0a73e8a20614177c2bdc
hawk <guinan.lin@foxmail.com>
2024-06-24 19:32:49 +0800

    add vue3&primevue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.datangyuan.cn",
-  integrations: [tailwind({ configFile: "./tailwind.config.mjs" })],
+  integrations: [tailwind({ configFile: "./tailwind.config.mjs" }),vue({jsx: true, reactivityTransform: true,appEntrypoint: '/src/main.js',})],
 });
